ba15ba06b104e6748d368f8ed92ada635a6fb868
Uvison-shop <72042917+Uvison-shop@users.noreply.github.com>
2020-12-02 20:39:35 +0100

    fgfg

--- simplifed.js
+++ simplifed.js
@@ -1,1 +1,22 @@
 console.log("test");
+
+var canvas;
+var ctx = CanvasRenderingContext2D.prototype;
+
+function start() {
+    canvas = document.createElement("canvas");
+    canvas.width = "500px";
+    canvas.height = "500px";
+    ctx = canvas.getContext("2d");
+    
+
+    setInterval(update, 30);
+}
+
+function rect(x,y, width, height, color) {
+    
+    ctx.rect(x,y,width,height);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
